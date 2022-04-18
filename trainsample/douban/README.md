0c99b440a76fb4b692287bb5993e4ee2040296f3
lixiang.2533 <lixiang.2533@bytedance.com>
2022-04-18 14:27:49 +0800

    update ignore add douban trainsample
    Change-Id: Iad379f9ac41e9e3d54a094c91e76cc63d990a60a

---
+++ trainsample/douban/README.md
@@ -0,0 +1,7 @@
+## 豆瓣电影影评
+
+1. 8 部电影，超 70 万 用户，超 200 万条 评分/评论 数据, 评分为[0,5]分
+
+2. 数据来源：https://github.com/SophonPlus/ChineseNlpCorpus/blob/master/datasets/dmsc_v2/intro.ipynb
+
+3. 下载链接：https://github.com/SophonPlus/ChineseNlpCorpus/blob/master/datasets/dmsc_v2/intro.ipynb
---
+++ trainsample/douban/preprocess.py
@@ -0,0 +1,21 @@
+# -*-coding:utf-8 -*-
+import pandas as pd
+import os
+from trainsample.converter import single_text
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    data_dir = './trainsample/douban'
+    df = pd.read_csv(os.path.join(data_dir, 'ratings.csv'))
+
+    train, test = train_test_split(df, test_size=0.2)
+    train, valid = train_test_split(train, test_size=0.2)
+
+    single_text(train['comment'], train['rating'], os.path.join(data_dir, 'train.txt'))
+    single_text(valid['comment'], valid['rating'], os.path.join(data_dir, 'valid.txt'))
+    single_text(test['comment'], test['rating'], os.path.join(data_dir, 'test.txt'))
+
+
+if __name__ == '__main__':
+    main()
