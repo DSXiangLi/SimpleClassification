4a627e6d2926f3f7ced8d9eb7572fe99af45b6ee
lixiang.2533 <lixiang.2533@bytedance.com>
2022-04-30 11:39:24 +0800

    add labeled_size parmas for semi-supervised
    Change-Id: If023e243a8af9cd2dc22bbddf166e11aea82ada5

--- model/temporal.py
+++ model/temporal.py
@@ -8,9 +8,9 @@ import tensorflow as tf
 from model.train_helper import build_model_fn, Trainer, BaseEncoder
 from tools.train_utils import HpParser, add_layer_summary
 
-hp_list = [HpParser.hp('temporal_alpha', 0.6), #  ensemble alpha
-           HpParser.hp('max_unsupervised', 1.0), #  w_max, when there is no unlabled data set it to 1
-           HpParser.hp('ramp_up_method', 'sigmoid') #  ramp up decay type
+hp_list = [HpParser.hp('temporal_alpha', 0.6),  # ensemble alpha
+           HpParser.hp('max_unsupervised', 1.0),  # w_max, when there is no unlabled data set it to 1
+           HpParser.hp('ramp_up_method', 'sigmoid')  # ramp up decay type
            ]
 hp_parser = HpParser(hp_list)
 
@@ -19,6 +19,7 @@ def ramp_up(cur_epoch, max_epoch, method):
     """
     根据训练epoch来调整无标注loss部分的权重，初始epoch无标注loss权重为0
     """
+
     def linear(cur_epoch, max_epoch):
         return cur_epoch / max_epoch
 
@@ -60,7 +61,8 @@ class TemporalWrapper(BaseEncoder):
         self.params = params
         self.encoder.params = params
         self.alpha = self.params['temporal_alpha']
-        self.wmax = tf.cast(self.params['max_unsupervised'] * self.params['labeled_size'] / self.params['sample_size'], tf.float32)
+        self.wmax = tf.cast(self.params['max_unsupervised'] * self.params['labeled_size'] / self.params['sample_size'],
+                            tf.float32)
 
         embedding = self.encode(features, is_training)
 
@@ -75,7 +77,7 @@ class TemporalWrapper(BaseEncoder):
             add_layer_summary('temporal_ensemble', temporal_ensemble)
 
             self.Z = tf.nn.embedding_lookup(temporal_ensemble, features['idx'])  # batch_size * label_size
-            self.Z = self.alpha * self.Z + (1-self.alpha) * preds
+            self.Z = self.alpha * self.Z + (1 - self.alpha) * preds
             self.assign_op = tf.scatter_update(temporal_ensemble, features['idx'], self.Z)
             add_layer_summary('ensemble', self.Z)
         return preds, labels
@@ -90,16 +92,17 @@ class TemporalWrapper(BaseEncoder):
 
     def compute_loss(self, predictions, labels):
         # supervised loss
-        mask = tf.cast(tf.where(labels>=0, tf.ones_like(labels), tf.zeros_like(labels)), tf.float32) # select labeled sample
+        mask = tf.cast(tf.where(labels >= 0, tf.ones_like(labels), tf.zeros_like(labels)),
+                       tf.float32)  # select labeled sample
         loss = self.params['loss_func'](predictions, labels)
         supervised_loss = tf.reduce_mean(loss * mask)
         add_layer_summary('supervised_mask', mask)
         tf.summary.scalar('loss/supervised_loss', supervised_loss)
 
         # MSE between current prediction and temporal ensemble
-        cur_epoch = tf.cast(tf.train.get_or_create_global_step()/self.params['steps_per_epoch'], tf.int32)
-        temporal_logits = self.Z/(1-tf.pow(self.alpha, tf.cast(cur_epoch+1, tf.float32)))  # startup bias
-        mse_loss = tf.reduce_mean((tf.nn.softmax(predictions) - tf.nn.softmax(temporal_logits))**2)
+        cur_epoch = tf.cast(tf.train.get_or_create_global_step() / self.params['steps_per_epoch'], tf.int32)
+        temporal_logits = self.Z / (1 - tf.pow(self.alpha, tf.cast(cur_epoch + 1, tf.float32)))  # startup bias
+        mse_loss = tf.reduce_mean((tf.nn.softmax(predictions) - tf.nn.softmax(temporal_logits)) ** 2)
         tf.summary.scalar('loss/mse_loss', mse_loss)
 
         # Loss加权
@@ -109,11 +112,33 @@ class TemporalWrapper(BaseEncoder):
         return total_loss
 
 
+def get_temporal_dataset(dataset_cls):
+    class TemporalDataset(dataset_cls):
+        def __init__(self, *args, **kwargs):
+            super(TemporalDataset, self).__init__(*args, **kwargs)
+
+        @property
+        def labeled_size(self):
+            return sum([int(i['label'] >= 0) for i in self.raw_data])
+
+        def update_params(self, train_params):
+            """
+            add labeled sample size for semi-supervised problem
+            """
+            train_params = super().update_params(train_params)
+            train_params.update(
+                {'labeled_size': self.labeled_size}
+            )
+            return train_params
+
+    return TemporalDataset
+
+
 def get_trainer(model):
     module = importlib.import_module('model.{}.model'.format(model))
     encoder = getattr(module, '{}Encoder'.format(model.capitalize()))
     dataset = getattr(module, 'dataset')
 
     trainer = Trainer(model_fn=build_model_fn(TemporalWrapper(encoder())),
-                      dataset_cls=dataset)
+                      dataset_cls=get_temporal_dataset(dataset))
     return trainer
