c40e49d63ae7a1320606c117e8c671901d872352
lixiang.2533 <lixiang.2533@bytedance.com>
2022-04-30 14:13:19 +0800

    change level in default logger
    Change-Id: Ie1eb821a48703c358f56624a24e82627fb5a427a

--- tools/logger.py
+++ tools/logger.py
@@ -7,15 +7,18 @@ import logging
 def get_logger(name, log_dir=None, log_level=logging.INFO):
     logger = logging.Logger(name)
     logger.setLevel(log_level)
-    formater = logging.Formatter('')
+    formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler = logging.StreamHandler()
     handler.setFormatter(formater)
+    handler.setLevel(logging.DEBUG)
     logger.addHandler(handler)
     if log_dir:
         # if log dir not provided, use stream handler only.
         handler = logging.FileHandler(os.path.join(log_dir, 'train.log'), 'a')
         handler.setFormatter(formater)
+        handler.setLevel(logging.INFO)
         logger.addHandler(handler)
+
     return logger
 
 
