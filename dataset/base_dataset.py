7419e222d4bdb69e66e72dea807be1b3a28a2948
lixiang.2533 <lixiang.2533@bytedance.com>
2022-05-02 10:36:43 +0800

    add tfrecord and img dataset
    Change-Id: Ia776e95faf9e58d3d62e396aeab0a1af2e0570e8

--- dataset/base_dataset.py
+++ dataset/base_dataset.py
@@ -4,6 +4,7 @@ import pickle
 import json
 import tensorflow as tf
 from tools.logger import logger
+from dataset.tfrecord_tools import TFRecordDump
 
 
 class BaseDataset(object):
@@ -12,7 +13,7 @@ class BaseDataset(object):
         self.batch_size = batch_size
         self.raw_data = []
         self.samples = []
-        self.dtypes = {'idx': tf.int32} # sample id
+        self.dtypes = {'idx': tf.int32}  # sample id
         self.shapes = {'idx': []}
         self.pads = {'idx': -1}
         self.feature_names = ['idx']
@@ -25,7 +26,7 @@ class BaseDataset(object):
 
     @property
     def steps_per_epoch(self):
-        return int(self.sample_size/self.batch_size)
+        return int(self.sample_size / self.batch_size)
 
     def build_proto(self):
         raise NotImplementedError
@@ -49,7 +50,7 @@ class BaseDataset(object):
         receiver_tensor = {}
         for i in self.feature_names:
             receiver_tensor[i] = tf.placeholder(dtype=self.dtypes[i],
-                                                shape=[None]+self.shapes[i],
+                                                shape=[None] + self.shapes[i],
                                                 name=i)
         return tf.estimator.export.ServingInputReceiver(receiver_tensor, receiver_tensor)
 
@@ -124,9 +125,9 @@ class GeneratorDataset(BaseDataset):
             dtypes = ({i: self.dtypes[i] for i in self.feature_names},
                       {i: self.dtypes[i] for i in self.label_names})
             pads = ({i: self.pads[i] for i in self.feature_names},
-                      {i: self.pads[i] for i in self.label_names})
+                    {i: self.pads[i] for i in self.label_names})
             dataset = tf.data.Dataset.from_generator(
-                lambda : self.build_generator(),
+                lambda: self.build_generator(),
                 output_shapes=shapes,
                 output_types=dtypes
             )
@@ -134,13 +135,89 @@ class GeneratorDataset(BaseDataset):
                 dataset = dataset.shuffle(int(self.batch_size * 5)).repeat()
 
             if not unbatch:
-                dataset = dataset.padded_batch(self.batch_size, shapes, pads).\
+                dataset = dataset.padded_batch(self.batch_size, shapes, pads). \
                     prefetch(tf.data.experimental.AUTOTUNE)
 
             return dataset
+
         return helper
 
 
+class TFRecordDataset(BaseDataset):
+    """
+    每次只读取instance_per_shard数据，转换特征并写tfrecord
+    """
 
+    def __init__(self, data_dir, batch_size):
+        super(TFRecordDataset, self).__init__(data_dir, batch_size)
+        self.tf_files = None
+        self.tf_dumper = None
+        self.tfrecord_names = []
+        self.build_proto()
 
+    @property
+    def tfrecord_type(self):
+        """
+        For int in feature type must be converted to int64
+        """
+        types = {}
+        for i in self.tfrecord_names + self.label_names:
+            if self.dtypes[i] == tf.int32:
+                types[i] = tf.int64
+            else:
+                types[i] = self.dtypes[i]
+        return types
 
+    def dump_tfrecord(self, file_name, instance_per_shard):
+        """
+        Here Filename is train/test/valid
+        """
+        self.load_data(file_name)
+        self.tf_dumper = TFRecordDump(self.data_dir, instance_per_shard, self.tfrecord_type)
+
+        shard = 0
+        samples = []
+        for i, data in enumerate(self.raw_data):
+            samples.append(self.build_single_feature(data))
+            if i // instance_per_shard != shard:
+                self.tf_dumper.dump(samples, file_name, shard)
+                shard += 1
+                samples = []
+        if samples:
+            self.tf_dumper.dump(samples, file_name, shard)
+
+    def parser(self, line, is_predict):
+        raise NotImplementedError()
+
+    def build_feature(self, file_name):
+        """
+        对于tfrecord类型，build feature不需要重新生成特征只需要读入parameters
+        """
+        self.load_data(file_name)
+        self.tf_files = os.path.join(self.data_dir, file_name + "_*.tfrecord")
+
+    def build_input_fn(self, is_predict=False, unbatch=False):
+        def helper():
+            shapes = ({i: self.shapes[i] for i in self.feature_names},
+                      {i: self.shapes[i] for i in self.label_names})
+            pads = ({i: self.pads[i] for i in self.feature_names},
+                    {i: self.pads[i] for i in self.label_names})
+
+            # avoid record shuffle in prediction mode
+            tfrecords = tf.data.Dataset.list_files(self.tf_files, shuffle=not is_predict)
+
+            dataset = tfrecords.interleave(tf.data.TFRecordDataset, cycle_length=1 if is_predict else 3)
+            dataset = dataset.map(
+                lambda x: self.parser(x, not is_predict),
+                num_parallel_calls=tf.data.experimental.AUTOTUNE
+            )
+
+            if not is_predict:
+                dataset = dataset.shuffle(int(self.batch_size * 5)).repeat()
+
+            if not unbatch:
+                dataset = dataset.padded_batch(self.batch_size, shapes, pads). \
+                    prefetch(tf.data.experimental.AUTOTUNE)
+            return dataset
+
+        return helper
---
+++ dataset/img_datasets.py
@@ -0,0 +1,65 @@
+# -*-coding:utf-8 -*-
+import tensorflow as tf
+import os
+
+from dataset.base_dataset import TFRecordDataset
+
+
+class ImgDataset(TFRecordDataset):
+    def __init__(self, data_dir, batch_size, img_size, img_preprocess):
+        super(ImgDataset, self).__init__(data_dir, batch_size)
+        self.channel_size = 3  # by Default no need to change
+        self.img_size = img_size
+        self.preprocess = img_preprocess  # only need in training not in TFRecord Dump
+
+    def build_proto(self):
+        self.dtypes.update({
+            'img_array': tf.float32,
+            'img_bytes': tf.string,
+            'label': tf.int32
+        })
+        self.shapes.update({
+            'img_array': [None, None, self.channel_size],
+            'label': []
+        })
+        self.pads.update({
+            'img_array': 0.0,
+            'label': 0
+
+        })
+        self.feature_names +=['img_array']
+        self.label_names +=['label']
+
+    def build_single_feature(self, data):
+        """
+        用gid去读区对应目录下images/gid.jpg文件, 返回img binary
+        这里没有直接convert成Numpy再tostring，用binary存储size要小10倍左右
+        """
+        gid = data['gid']
+        with tf.gfile.GFile(os.path.join(self.data_dir, 'images', gid + '.jpg'), 'rb') as fid:
+            img_bytes = fid.read()
+        return {
+            'img_bytes': img_bytes,
+            'label': int(data['label'])
+        }
+
+    def parser(self, line, is_predict):
+        proto = {
+            'img_bytes': tf.io.FixedLenFeature([], dtype=tf.string),
+            'label': tf.io.FixedLenFeature([], dtype=tf.int64)
+        }
+        feature = tf.parse_single_example(line, features=proto)
+        img_array = tf.io.decode_jpeg(feature.pop('img_bytes'), channels=self.channel_size)  # image of arbitary size
+        img_array = self.preprocess(img_array, self.img_size, self.img_size,
+                                    is_training=(not is_predict))
+        feature['img_array'] = tf.reshape(img_array, [self.img_size, self.img_size, self.channel_size])
+        label = {'label': tf.cast(feature.pop('label'), tf.int32)}
+        return feature, label
+
+    def update_params(self, train_params):
+        train_params.update({
+            'sample_size': self.sample_size,
+            'steps_per_epoch': self.steps_per_epoch,
+            'num_train_steps': int(self.steps_per_epoch * train_params['epoch_size'])
+        })
+        return train_params
