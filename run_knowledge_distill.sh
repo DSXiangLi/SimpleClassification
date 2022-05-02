5e8e1582d88a50e9f3f21c6a265d2aca45b36031
lixiang.2533 <lixiang.2533@bytedance.com>
2022-05-02 10:45:47 +0800

    update knowledge distill bash
    Change-Id: I725095193db370da01909024be28bfc53e6ac400

--- run_knowledge_distill.sh
+++ run_knowledge_distill.sh
@@ -1,6 +1,6 @@
-DATA_DIR='waimai'
-CKPT='waimai_bert_v1'
-MAX_SEQ_LEN=150
+DATA_DIR='chnsenticorp'
+CKPT='chnsenticorp_bert'
+MAX_SEQ_LEN=300
 LABEL_SIZE=2
 TEACHER_SURFIX='teacher'
 
@@ -18,39 +18,68 @@ python main.py \
   --label_size $LABEL_SIZE\
   --use_gpu \
   --device 6\
-  --clear_model\
-  --do_train\
-  --do_eval\
   --do_predict\
+  --predict_file 'all'
 
 ## Split all into train/test/valid
-echo 'Shuffle and split all samples into train/test'
+echo 'Split all samples into train/test'
 python ./trainsample/converter.py \
   --data_dir "./trainsample/${DATA_DIR}"\
   --input_file "${CKPT}_all"\
   --output_surfix $TEACHER_SURFIX
 
-## Use Knowledge Distill to train student
+
+## Train small model for comparison
+echo 'Train small model directly'
 python main.py \
-  --model 'fasttext'\
+  --model 'textcnn'\
   --loss 'ce'\
   --data_dir $DATA_DIR\
-  --nlp_pretrain_model 'word2vec_baike'\
-  --ckpt_dir 'distill_bert2fasttext'\
-  --epoch_size 5\
-  --batch_size 32\
-  --knowledge_distill\
-  --distill_weight 0.5\
-  --distill_loss 'ce'\
-  --temperature 2\
+  --nlp_pretrain_model 'word2vec_baike' \
+  --ckpt_dir "${DATA_DIR}_textcnn" \
+  --epoch_size 10\
+  --batch_size 64\
+  --max_seq_len $MAX_SEQ_LEN\
+  --label_size $LABEL_SIZE\
+  --lr 1e-3\
+  --filter_list '100,100,100'\
+  --kernel_size_list '3,4,5'\
+  --cnn_activation 'relu'\
+  --use_gpu \
+  --device 6\
   --train_file "train_${TEACHER_SURFIX}"\
   --valid_file "valid_${TEACHER_SURFIX}"\
   --eval_file "test_${TEACHER_SURFIX}"\
+  --clear_model\
+  --do_train\
+  --do_eval\
+
+## Use Knowledge Distill to train student
+echo 'Distill Big Model to Small Model'
+python main.py \
+  --model 'textcnn'\
+  --loss 'ce'\
+  --data_dir $DATA_DIR\
+  --nlp_pretrain_model 'word2vec_baike'\
+  --ckpt_dir "${DATA_DIR}_distill_bert2textcnn"\
+  --epoch_size 30\
+  --batch_size 64\
+  --knowledge_distill\
+  --distill_weight 6\
+  --distill_loss 'ce'\
+  --temperature 5\
   --max_seq_len $MAX_SEQ_LEN\
-  --lr 1e-3\
   --label_size $LABEL_SIZE\
+  --lr 1e-4\
+  --save_steps 50\
+  --filter_list '100,100,100'\
+  --kernel_size_list '3,4,5'\
+  --cnn_activation 'relu'\
   --use_gpu \
   --device 6\
-  --do_train\
+  --train_file "train_${TEACHER_SURFIX}"\
+  --valid_file "valid_${TEACHER_SURFIX}"\
+  --eval_file "test_${TEACHER_SURFIX}"\
   --clear_model\
+  --do_train\
   --do_eval
